6b9484a4af445d177e5bf7e31640e3b06517f219
apolinarii <borodesku@gmail.com>
2022-05-06 16:49:09 +0300

    two commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + firstName + middleName;
+        System.out.println(fullName);
+
+        String fullNameUp = fullName.toUpperCase();
+        System.out.println(fullNameUp);
+
+        String fullNameRu = "Семёнов Семён Семёнович";
+        String[] words = fullNameRu.split(" ");
+        for (int i = 0; i < words.length; i++) words[i] = words[i].replace("ё", "e");
+        String resultFullName = Arrays.toString(words)
+                .replace("[", "")
+                .replace("]", "")
+                .replace(",", "");
+        System.out.println("Данные ФИО сотрудника — " + resultFullName);
+
 
 
 
